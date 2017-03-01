233fe93f241f4a6e3480d267ec04ce02202b8964
phmurray <philip.murray@capgemini.com>
2017-03-01 16:42:40 +0000

    Baseline commit of existing files

---
+++ src/MyThing.java
@@ -0,0 +1,20 @@
+
+public class MyThing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 9;
+		double b = 10;
+		
+		String s1 = "Colonel ";
+		String s2 = "Samantha";
+		String captain = s1 + s2;
+		System.out.println(captain);
+		
+		System.out.println(a + b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a - b);
+	}
+
+}
